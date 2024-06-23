94299ff0ae9cc621614b4620aeb136d6f1a55af2
Kostya <betenya.kostya@mail.ru>
2024-06-23 16:19:56 +0300

    tasks 9-11 complete

---
+++ 2semester/10/Task10.dpr
@@ -0,0 +1,82 @@
+﻿Program Task10;
+
+(*
+  Дан массив из n элементов, n > 5. В каждом компоненте массива 
+  записано имя человека, кол-во лайков и сообщений и друзей.
+  (Вывести 5 человек, у которых наибольшее количество лайков)
+
+  P.S. Текстовый файл
+*)
+
+(*
+  Файл:
+
+  8
+  1
+  1 1 1
+  2
+  2 2 2
+  3
+  3 3 3
+  4
+  4 4 4
+  5
+  5 5 5
+  6
+  6 6 6
+  7
+  7 7 7
+  8
+  8 8 8
+*)
+
+{$APPTYPE CONSOLE}
+{$R *.res}
+
+Type
+    TPerson = Record
+        Name: String;
+        Likes, Messages, Friends: Integer;
+    End;
+
+    TArr = Array Of TPerson;
+
+Procedure Sort(Var Arr: TArr);
+Begin
+    For Var I := Low(Arr) To Low(Arr) + 4 Do
+    Begin
+        Var
+        J := I + 1;
+        While (J < Length(Arr)) And (Arr[J].Likes > Arr[I].Likes) Do
+        Begin
+            Var
+            Temp := Arr[I];
+            Arr[I] := Arr[J];
+            Arr[J] := Temp;
+            Inc(J);
+        End;
+    End;
+End;
+
+Var
+    Arr: TArr;
+    N: Integer;
+    F: TextFile;
+
+Begin
+    Assign(F, 'test.txt');
+    Reset(F);
+    Readln(F, N);
+    SetLength(Arr, N);
+    For Var I := Low(Arr) To High(Arr) Do
+    Begin
+        Readln(F, Arr[I].Name);
+        Read(F, Arr[I].Likes, Arr[I].Messages, Arr[I].Friends);
+        Readln(F);
+    End;
+    Sort(Arr);
+    For Var I := Low(Arr) To Low(Arr) + 4 Do
+        Write(Arr[I].Name, ' ');
+    Readln;
+
+End.
